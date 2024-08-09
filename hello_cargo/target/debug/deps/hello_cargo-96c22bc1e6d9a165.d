859765e81e9d14e66b2d768a25d3e8db882c955e
ThiagoLahass <lahassthiagofelippe@gmail.com>
2024-08-09 12:41:29 -0300

    :tada:Initial commit

---
+++ hello_cargo/target/debug/deps/hello_cargo-96c22bc1e6d9a165.d
@@ -0,0 +1,5 @@
+/home/thiago_lahass/Documents/learning-rust/hello_cargo/target/debug/deps/hello_cargo-96c22bc1e6d9a165: src/main.rs
+
+/home/thiago_lahass/Documents/learning-rust/hello_cargo/target/debug/deps/hello_cargo-96c22bc1e6d9a165.d: src/main.rs
+
+src/main.rs:
